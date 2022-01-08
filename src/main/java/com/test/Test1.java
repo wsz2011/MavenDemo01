e34d257d2f69c6c11cc15894d288965c4e924fc9
zhb_golbal <807933196@qq.com>
2022-01-08 15:12:44 +0800

    Maven push

---
+++ src/main/java/com/test/Test1.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Test1 {
+	public static void main(String[] args) {
+		SayHello();
+	}
+	
+	public static void SayHello(){
+		System.out.println("Hi,have a good day!");
+	}
+}
+
